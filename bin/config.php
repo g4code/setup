2302dbf8bf747623a7add2668aa45e5bb508812d
Drasko Gomboc <drasko.gomboc@gmail.com>
2018-12-20 12:22:26 +0100

    Composer require packages: config, value-object and garden-cli; bin config

--- bin/config.php
+++ bin/config.php
@@ -1,2 +1,27 @@
 <?php
 
+$possibleAutoloadPaths = [
+    join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'autoload.php']),
+    join(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', '..', 'autoload.php']),
+    join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'vendor', 'autoload.php'])
+];
+
+$autoloadPath = null;
+
+foreach ($possibleAutoloadPaths as $oneAutoloadPath) {
+    if (file_exists($oneAutoloadPath)) {
+        $autoloadPath = $oneAutoloadPath;
+        break;
+    }
+}
+
+if ($autoloadPath === null) {
+    fputs(STDERR, "Missing autoload file!\n");
+    exit(1);
+}
+
+require_once realpath($autoloadPath);
+
+use Garden\Cli\Cli;
+
+
---
+++ tests/unit/src/ConfigTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace G4\Setup\Test\Unit;
+
+
+use G4\Setup\Config;
+
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+}
